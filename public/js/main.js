49aea2eb2223bcf57ba816ca3f8fbe2758ba9ef8
Stacey Seronick <sseronic@uci.edu>
2017-06-02 23:43:26 -0700

    fixed order of messages

--- public/js/main.js
+++ public/js/main.js
@@ -88,7 +88,7 @@ socket.on('join_room_response',function(payload){
 	var newHTML = '<p>'+payload.username+' just entered the lobby</p>';
 	var newNode = $(newHTML);
 	newNode.hide();
-	$('#messages').append(newNode);
+	$('#messages').prepend(newNode);
 	newNode.slideDown(1000);
 });
 
@@ -114,10 +114,10 @@ socket.on('player_disconnected',function(payload){
 	}
 
 	/* Manage the message that a player has left */
-	var newHTML = '<p>'+payload.username+' has left the lobby</p>';
+	var newHTML = '<p>'+payload.username+' has left the room</p>';
 	var newNode = $(newHTML);
 	newNode.hide();
-	$('#messages').append(newNode);
+	$('#messages').prepend(newNode);
 	newNode.slideDown(1000);
 });
 
@@ -216,6 +216,7 @@ function send_message(){
 	payload.message = $('#send_message_holder').val();
 	console.log('*** Client Log Message: \'send_message\' payload: '+JSON.stringify(payload));
 	socket.emit('send_message',payload);
+	$('#send_message_holder'.val('');
 }
 
 socket.on('send_message_response',function(payload){
@@ -227,7 +228,7 @@ socket.on('send_message_response',function(payload){
 	var newHTML = '<p><b>'+payload.username+' says</b> '+payload.message+'</p>';
 	var newNode = $(newHTML);
 	newNode.hide();
-	$('#messages').append(newNode);
+	$('#messages').prepend(newNode);
 	newNode.slideDown(1000);
 });
 
